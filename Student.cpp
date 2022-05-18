d3a5607510858d3593349b8d8d8cb0d173473207
IT21376986 <100300719+IT21376986@users.noreply.github.com>
2022-05-18 22:01:22 +0530

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -10,3 +10,19 @@ Student::assignDetails() {
 Student::display() {
   
 }
+
+#include "Student.h"
+#include <cstring>
+#include <iostream>
+
+// Assign studentId and name
+Student :: assignDetails() {
+	studentID = 1010;
+	strcpy(name, "kamal");
+}
+
+// Display StudentId and Name
+Student :: display() {
+	cout<<"student id : " <<studentID<<endl;
+	cout<<"student name : " <<name<<endl;
+}
